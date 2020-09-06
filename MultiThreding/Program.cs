7968017bd687cc5a64262f2780f3297db5327f4f
Rahul K <rahulgskumar12@gmail.com>
2020-09-06 11:55:14 +0530

    Add project files.

---
+++ MultiThreding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiThreding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
