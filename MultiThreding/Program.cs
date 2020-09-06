307c17016ace1915119f8d2c5a03ad61ff52431a
Rahul K <rahulgskumar12@gmail.com>
2020-09-06 12:06:43 +0530

    parallel.invoke() commit

--- MultiThreding/Program.cs
+++ MultiThreding/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreding
 {
     class Program
     {
+        static void task1()
+        {
+            Console.WriteLine("Task 1 started");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task 1 finished");
+        }
+        static void task2()
+        {
+            Console.WriteLine("Task 2 started");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task 2 finished");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parallel.Invoke(() => task1(), () => task2());
+            Console.WriteLine("Task Finished, Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
